4e5fbb052c5466e599ff0d38860f582be0487072
Shohan Hasan <shohan.hasan.x6@gmail.com>
2019-09-01 17:18:20 -0400

    Use url to get API file

--- yard_script.sh
+++ yard_script.sh
@@ -1,11 +1,13 @@
+#!/usr/bin/env bash
 # Script to copy over Datavyu_API.rb from datavyu src folder, generate html file, and remove the src file
 
-in_path=../datavyu/src/main/resources/Datavyu_API.rb
-out_folder=source/_static/yard
-copy_name=Datavyu_API.rb
+# in_path="../datavyu/src/main/resources/Datavyu_API.rb"
+api_file_loc="https://raw.githubusercontent.com/databrary/datavyu/dev/src/main/resources/Datavyu_API.rb"
+out_folder="./docs"
+copy_name="Datavyu_API.rb"
 copy_path="${out_folder}/${copy_name}"
 
 mkdir -p "$out_folder"
-cp "$in_path" $copy_path
-cd "$out_folder" && yard --output-dir . "$copy_name"
+curl -fsSL --output "$copy_name" "$api_file_loc"
+yard --output-dir "$out_folder" "$copy_name"
 rm "$copy_name"
